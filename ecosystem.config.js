f0430f2fce7e79704e19ee7898a8545b1f33048d
deepanshuish <deepanshusharma204@gmail.com>
2026-01-03 03:36:19 +0530

    ko

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,9 +16,7 @@ module.exports = {
             args: 'index.js',
             env: {
                 NODE_ENV: 'production',
-                PORT: 5000,
-                DATABASE_URL: 'sqlserver://comuno.database.windows.net:1433;database=comuno;user=CloudSAe20540ec;password={Comuno@123};encrypt=true;trustServerCertificate=false;loginTimeout=30;',
-                JWT_SECRET: 'comuno_prod_secret_8822'
+                PORT: 5000
             }
         }
     ]
