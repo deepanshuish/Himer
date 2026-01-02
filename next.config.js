066dbb49d77f49c9367194b77c5b6b350350f69f
deepanshuish <deepanshusharma204@gmail.com>
2026-01-03 03:33:13 +0530

    right

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   env: {
     API_URL: process.env.API_URL || 'http://localhost:5000',
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:5000/api/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
